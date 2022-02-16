366f53ce2d953101b51ded258f121655088daf39
qgadrian <qgadrian@users.noreply.github.com>
2022-02-16 15:41:28 +0100

    Support sending `test_event_code`
    This commit also ensure the `data` multipart is encoded.

--- test/facebook_conversions/facebook_conversions_test.exs
+++ test/facebook_conversions/facebook_conversions_test.exs
@@ -26,10 +26,12 @@ defmodule FacebookConversionsTest do
       Mox.expect(Tesla.MockAdapter, :call, 1, fn %Tesla.Env{url: url, body: body}, _opts ->
         assert url =~ ~r/a_facebook_pixel_id\/events/
 
+        expected_events = Jason.encode!(events)
+
         assert %Tesla.Multipart{
                  content_type_params: ["charset=utf-8"],
                  parts: [
-                   %Tesla.Multipart.Part{body: ^events, dispositions: [name: "data"]},
+                   %Tesla.Multipart.Part{body: ^expected_events, dispositions: [name: "data"]},
                    %Tesla.Multipart.Part{
                      body: "an_access_token",
                      dispositions: [name: "access_token"]
@@ -41,6 +43,51 @@ defmodule FacebookConversionsTest do
       end)
 
       assert {:ok, :success} = FacebookConversions.send_events(events)
+    end
+
+    test "sends the test even code" do
+      event = %Event{
+        event_name: "a facebook event",
+        event_time: 23,
+        action_source: "system_generated",
+        custom_data: %{"ping" => "pong"},
+        user_data: %UserData{
+          em: ["user_email_hased"],
+          client_ip_address: "the ip",
+          client_user_agent: "007",
+          external_id: ["user_id_hashed"],
+          fbc: "facebook campaign",
+          fbp: "facebook psomething"
+        }
+      }
+
+      events = [event]
+
+      Mox.expect(Tesla.MockAdapter, :call, 1, fn %Tesla.Env{url: url, body: body}, _opts ->
+        assert url =~ ~r/a_facebook_pixel_id\/events/
+
+        expected_events = Jason.encode!(events)
+
+        assert %Tesla.Multipart{
+                 content_type_params: ["charset=utf-8"],
+                 parts: [
+                   %Tesla.Multipart.Part{body: ^expected_events, dispositions: [name: "data"]},
+                   %Tesla.Multipart.Part{
+                     body: "an_access_token",
+                     dispositions: [name: "access_token"]
+                   },
+                   %Tesla.Multipart.Part{
+                     body: "a test event code",
+                     dispositions: [name: "test_event_code"]
+                   }
+                 ]
+               } = body
+
+        {:ok, %Tesla.Env{status: 200, body: :success}}
+      end)
+
+      assert {:ok, :success} =
+               FacebookConversions.send_events(events, test_event_code: "a test event code")
     end
   end
 end
